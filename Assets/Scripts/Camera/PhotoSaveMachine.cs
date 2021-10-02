14d762abfad9415f5fd74b56886077320cdaa486
request <iamrequest@protonmail.com>
2021-10-02 15:54:40 -0400

    Modeld save machine

---
+++ Assets/Scripts/Camera/PhotoSaveMachine.cs
@@ -0,0 +1,14 @@
+﻿using HurricaneVR.Framework.Core;
+using HurricaneVR.Framework.Core.Grabbers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotoSaveMachine : MonoBehaviour {
+    public void TrySavePhoto(HVRGrabberBase hvrGrabberBase, HVRGrabbable hvrGrabbable) {
+        if (hvrGrabbable.TryGetComponent(out Photo photo)) {
+            // TODO: Play animation, display "saved!" message
+            photo.SaveToFile();
+        }
+    }
+}
