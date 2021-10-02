c0262ae113d43ada28a93f4d364b8ab1ad0f9a75
request <iamrequest@protonmail.com>
2021-10-02 10:26:20 -0400

    Added base infrastructure for photo capture
    - Photo capture
    	- Added a simple camera that renders to a RenderTexture, which gets saved as a gameobject in-game.
    	- Added the functionality to write a Photo gameobject to the filesystem as a PNG
    - Also started working on height calibration, still a WIP

---
+++ Assets/Scripts/Camera/CameraRoll.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class CameraRoll : MonoBehaviour {
+    [AssetsOnly]
+    public GameObject photoPrefab;
+    public List<Photo> photos;
+
+    public void AddPhoto(RenderTexture rt) {
+        GameObject newPhotoGameObject = Instantiate(photoPrefab, transform.position, transform.rotation);
+        Photo newPhoto = newPhotoGameObject.GetComponent<Photo>();
+
+        newPhoto.texture = rt;
+        newPhoto.image.texture = newPhoto.texture;
+        newPhoto.timestamp = System.DateTime.Now;
+    }
+}
---
+++ Assets/Scripts/Player/HeightCalibrator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RootMotion.FinalIK;
+using Sirenix.OdinInspector;
+
+// See also: VRIKCalibrationController
+public class HeightCalibrator : MonoBehaviour {
+    public VRIK vrik;
+    public VRIKCalibrator.Settings settings;
+    //public VRIKCalibrator.CalibrationData calibrationData;
+
+    [Button]
+    public void Calibrate() {
+        VRIKCalibrator.Calibrate(vrik, settings,
+            vrik.solver.spine.headTarget,
+            null,
+            vrik.solver.leftArm.target,
+            vrik.solver.rightArm.target);
+    }
+
+    public void CalibrateScale() {
+    }
+}
