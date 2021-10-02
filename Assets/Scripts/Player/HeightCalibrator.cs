c6b15d3ee755179e0b3026a19d33ade1d96f583c
request <iamrequest@protonmail.com>
2021-10-02 10:53:32 -0400

    Fixed full body height scaling
    - Hands no longer rotate on calibration
    	- Turns out I shouldn't use the anchor transforms from VRIK.Solver, and instead I should be referencing the transforms directly.
    	- This still has a different rotation bug if I remove the transforms on the VRIK solver, I'll probably have to scale the model manually

--- Assets/Scripts/Player/HeightCalibrator.cs
+++ Assets/Scripts/Player/HeightCalibrator.cs
@@ -7,18 +7,45 @@ using Sirenix.OdinInspector;
 // See also: VRIKCalibrationController
 public class HeightCalibrator : MonoBehaviour {
     public VRIK vrik;
+    public float scaleMultiplier;
+    public Transform leftHandAnchor, rightHandAnchor, hmdAnchor;
+
     public VRIKCalibrator.Settings settings;
-    //public VRIKCalibrator.CalibrationData calibrationData;
+    public VRIKCalibrator.CalibrationData calibrationData;
 
     [Button]
-    public void Calibrate() {
-        VRIKCalibrator.Calibrate(vrik, settings,
-            vrik.solver.spine.headTarget,
+    [HideInEditorMode]
+    public void CalibrateNew() {
+        calibrationData = VRIKCalibrator.Calibrate(vrik, 
+            settings,
+            hmdAnchor,
             null,
-            vrik.solver.leftArm.target,
-            vrik.solver.rightArm.target);
+            leftHandAnchor,
+            rightHandAnchor);
     }
 
+    [Button]
+    [HideInEditorMode]
+    public void CalibrateUsingExistingData() {
+        VRIKCalibrator.Calibrate(vrik, 
+            calibrationData, 
+            hmdAnchor, 
+            null, 
+            leftHandAnchor,
+            rightHandAnchor);
+    }
+
+    [Button]
+    [HideInEditorMode]
     public void CalibrateScale() {
+        if (calibrationData != null) {
+            VRIKCalibrator.RecalibrateScale(vrik, calibrationData, scaleMultiplier);
+        }
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.C)) {
+            CalibrateNew();
+        }
     }
 }
