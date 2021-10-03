6c264eb45eb891db03c9e29ae180ada292b11110
request <iamrequest@protonmail.com>
2021-10-03 15:25:40 -0400

    Fixed finalIK height bug
    - Adjusted player prefab so that the finalIK model would properly touch the ground once the player starts moving into different heights in the scene
    - Fixed avatar knee rotation during crouch

---
+++ Assets/Scripts/Player/HexabodyPivotFollower.cs
@@ -0,0 +1,22 @@
+﻿using HexabodyVR.PlayerController;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Transform that follows the hexasphere locosphere, positioning itself directly under the locosphere.
+/// This gameobject will function as the parent transform for the finalIK avatar
+/// </summary>
+public class HexabodyPivotFollower : MonoBehaviour {
+    public HexaBodyPlayer3 hexabodyRig;
+
+    private Vector3 locosphereOffset;
+
+    private void Start() {
+        locosphereOffset = new Vector3(0f, hexabodyRig.LocoCollider.radius, 0f);
+    }
+    void FixedUpdate() {
+        transform.position = hexabodyRig.LocoSphere.position;
+        transform.position -= locosphereOffset;
+    }
+}
