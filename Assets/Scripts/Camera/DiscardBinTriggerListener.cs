8f56901232ad5ffb89128b7cbd29db093ef4f668
request <iamrequest@protonmail.com>
2021-10-04 15:25:45 -0400

    Garbage bin now plays sfx properly

--- Assets/Scripts/Camera/DiscardBinTriggerListener.cs
+++ Assets/Scripts/Camera/DiscardBinTriggerListener.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using HurricaneVR.Framework.Core.Utils;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -6,9 +7,11 @@ using UnityEngine.Events;
 // Discard photos that get dropped in the pin, rather than placed
 // This could be refactored. There's no real need for a socket anyways
 public class DiscardBinTriggerListener : MonoBehaviour {
+    public AudioClip photoDeletedSFX;
     private void OnTriggerEnter(Collider other) {
         if (other.TryGetComponent(out Photo photo)) {
             if (!photo.hvrGrabbable.IsBeingHeld) {
+                SFXPlayer.Instance.PlaySFX(photoDeletedSFX, transform.position);
                 Destroy(photo.gameObject);
             }
         }
@@ -16,6 +19,7 @@ public class DiscardBinTriggerListener : MonoBehaviour {
         Photo photoInParent = other.GetComponentInParent<Photo>();
         if (photoInParent) {
             if (!photoInParent.hvrGrabbable.IsBeingHeld) {
+                SFXPlayer.Instance.PlaySFX(photoDeletedSFX, transform.position);
                 Destroy(photoInParent.gameObject);
             }
         }
